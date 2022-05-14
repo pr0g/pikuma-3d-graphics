1eaa05d766d1db596d0cfb6c0984e1c133a66ca2
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-05-14 22:40:57 +0100

    add initial SDL code

--- src/main.c
+++ src/main.c
@@ -1,8 +1,44 @@
 #include <SDL.h>
 
+#include <stdbool.h>
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-  printf("hello, world!\n");
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+
+bool initialize_window(void)
+{
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fprintf(stderr, "Error initializing SDL.\n");
+    return false;
+  }
+
+  window = SDL_CreateWindow(
+    NULL,
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    800,
+    600,
+    SDL_WINDOW_BORDERLESS);
+
+  if (!window) {
+    fprintf(stderr, "Error creating SDL window.\n");
+    return false;
+  }
+
+  renderer = SDL_CreateRenderer(window, -1, 0);
+
+  if (!renderer) {
+    fprintf(stderr, "Error creating SDL renderer.\n");
+    return false;
+  }
+
+  return true;
+}
+
+int main(int argc, char** argv)
+{
+  is_running = initialize_window();
   return 0;
 }
