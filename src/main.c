4d5f71651a4fe8a54ebf8c6b7275b33704cda31d
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-05-14 22:53:29 +0100

    add basic sdl display

--- src/main.c
+++ src/main.c
@@ -3,12 +3,11 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool is_running = false;
+// global data
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
-bool initialize_window(void)
-{
+bool initialize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
     fprintf(stderr, "Error initializing SDL.\n");
     return false;
@@ -37,8 +36,44 @@ bool initialize_window(void)
   return true;
 }
 
-int main(int argc, char** argv)
-{
-  is_running = initialize_window();
+void setup(void) {
+}
+
+bool process_input(void) {
+  SDL_Event event;
+  SDL_PollEvent(&event);
+  switch (event.type) {
+    case SDL_QUIT:
+      return false;
+    case SDL_KEYDOWN:
+      if (event.key.keysym.sym == SDLK_ESCAPE) {
+        return false;
+      }
+    default:
+      break;
+  }
+  return true;
+}
+
+void update(void) {
+}
+
+void render(void) {
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
+}
+
+int main(int argc, char** argv) {
+  bool is_running = initialize_window();
+
+  setup();
+
+  while (is_running) {
+    is_running = process_input();
+    update();
+    render();
+  }
+
   return 0;
 }
