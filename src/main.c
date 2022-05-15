abc0da795f9b0fc0cf3ba8e3a44d79a97e8d0b23
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-05-15 14:31:45 +0100

    update global variable names

--- src/main.c
+++ src/main.c
@@ -5,15 +5,14 @@
 #include <stdio.h>
 
 // global data
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
-
-uint32_t* color_buffer = NULL;
-SDL_Texture* color_buffer_texture = NULL;
+SDL_Window* g_window = NULL;
+SDL_Renderer* g_renderer = NULL;
+uint32_t* g_color_buffer = NULL;
+SDL_Texture* g_color_buffer_texture = NULL;
 
 // default width/height
-int window_width = 800;
-int window_height = 600;
+int g_window_width = 800;
+int g_window_height = 600;
 
 typedef struct
 {
@@ -31,42 +30,42 @@ bool initialize_window(void) {
 
   SDL_DisplayMode display_mode;
   SDL_GetCurrentDisplayMode(0, &display_mode);
-  window_width = display_mode.w;
-  window_height = display_mode.h;
+  g_window_width = display_mode.w;
+  g_window_height = display_mode.h;
 
-  window = SDL_CreateWindow(
+  g_window = SDL_CreateWindow(
     NULL,
     SDL_WINDOWPOS_CENTERED,
     SDL_WINDOWPOS_CENTERED,
-    window_width,
-    window_height,
+    g_window_width,
+    g_window_height,
     SDL_WINDOW_BORDERLESS);
 
-  if (!window) {
+  if (!g_window) {
     fprintf(stderr, "Error creating SDL window.\n");
     return false;
   }
 
-  renderer = SDL_CreateRenderer(window, -1, 0);
+  g_renderer = SDL_CreateRenderer(g_window, -1, 0);
 
-  if (!renderer) {
+  if (!g_renderer) {
     fprintf(stderr, "Error creating SDL renderer.\n");
     return false;
   }
 
-  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+  SDL_SetWindowFullscreen(g_window, SDL_WINDOW_FULLSCREEN);
 
   return true;
 }
 
 void setup(void) {
-  color_buffer = malloc(sizeof(uint32_t) * window_width * window_height);
-  color_buffer_texture = SDL_CreateTexture(
-    renderer,
+  g_color_buffer = malloc(sizeof(uint32_t) * g_window_width * g_window_height);
+  g_color_buffer_texture = SDL_CreateTexture(
+    g_renderer,
     SDL_PIXELFORMAT_ARGB8888,
     SDL_TEXTUREACCESS_STREAMING,
-    window_width,
-    window_height);
+    g_window_width,
+    g_window_height);
 }
 
 bool process_input(void) {
@@ -89,14 +88,14 @@ void update(void) {
 }
 
 void draw_grid(const int spacing, const int32_t color) {
-  for (int grid_col = 0; grid_col < window_width; grid_col += spacing) {
-    for (int row = 0; row < window_height; ++row) {
-      color_buffer[row * window_width + grid_col] = color;
+  for (int grid_col = 0; grid_col < g_window_width; grid_col += spacing) {
+    for (int row = 0; row < g_window_height; ++row) {
+      g_color_buffer[row * g_window_width + grid_col] = color;
     }
   }
-  for (int col = 0; col < window_width; ++col) {
-    for (int grid_row = 0; grid_row < window_height; grid_row += spacing) {
-      color_buffer[grid_row * window_width + col] = color;
+  for (int col = 0; col < g_window_width; ++col) {
+    for (int grid_row = 0; grid_row < g_window_height; grid_row += spacing) {
+      g_color_buffer[grid_row * g_window_width + col] = color;
     }
   }
 }
@@ -104,28 +103,31 @@ void draw_grid(const int spacing, const int32_t color) {
 void draw_rect(const rect_t* rect, int32_t color) {
   for (int y = rect->y; y < rect->y + rect->height; ++y) {
     for (int x = rect->x; x < rect->x + rect->width; ++x) {
-      color_buffer[y * window_width + x] = color;
+      g_color_buffer[y * g_window_width + x] = color;
     }
   }
 }
 
 void clear_color_buffer(const uint32_t color) {
-  for (int col = 0; col < window_width; ++col) {
-    for (int row = 0; row < window_height; ++row) {
-      color_buffer[row * window_width + col] = color;
+  for (int col = 0; col < g_window_width; ++col) {
+    for (int row = 0; row < g_window_height; ++row) {
+      g_color_buffer[row * g_window_width + col] = color;
     }
   }
 }
 
 void render_color_buffer(void) {
   SDL_UpdateTexture(
-    color_buffer_texture, NULL, color_buffer, window_width * sizeof(uint32_t));
-  SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+    g_color_buffer_texture,
+    NULL,
+    g_color_buffer,
+    g_window_width * sizeof(uint32_t));
+  SDL_RenderCopy(g_renderer, g_color_buffer_texture, NULL, NULL);
 }
 
 void render(void) {
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-  SDL_RenderClear(renderer);
+  SDL_SetRenderDrawColor(g_renderer, 255, 0, 0, 255);
+  SDL_RenderClear(g_renderer);
 
   draw_grid(10, 0xff444444);
   draw_rect(
@@ -134,13 +136,13 @@ void render(void) {
   render_color_buffer();
   clear_color_buffer(0xff000000);
 
-  SDL_RenderPresent(renderer);
+  SDL_RenderPresent(g_renderer);
 }
 
 void teardown(void) {
-  free(color_buffer);
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
+  free(g_color_buffer);
+  SDL_DestroyRenderer(g_renderer);
+  SDL_DestroyWindow(g_window);
   SDL_Quit();
 }
 
