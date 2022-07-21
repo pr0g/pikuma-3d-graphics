508346a840a8ee0b70981c5916eaf5a4806ce32c
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2022-07-21 21:44:36 +0100

    beginning of clip_polygon_against_plane

--- src/polygon.c
+++ src/polygon.c
@@ -14,7 +14,35 @@ polygon_t build_polygon_from_triangle(const triangle_t triangle) {
 
 static void clip_polygon_against_plane(
   polygon_t* polygon, const plane_t plane) {
-  // todo
+  point3f_t* inside_vertices = NULL;
+
+  point3f_t* current_vertex = &polygon->vertices[0];
+  point3f_t* const last_vertex =
+    &polygon->vertices[array_length(&polygon->vertices) - 1];
+  point3f_t* previous_vertex = last_vertex;
+
+  float previous_dot = vec3f_dot_vec3f(
+    point3f_sub_point3f(*previous_vertex, plane.point), plane.normal); // q2
+
+  while (current_vertex != last_vertex) {
+    const float current_dot = vec3f_dot_vec3f(
+      point3f_sub_point3f(*current_vertex, plane.point), plane.normal); // q1
+    // if we changed from inside to outside
+    if (current_dot * previous_dot < 0.0f) {
+      point3f_t intersection_point = {}; // // calculate intersection point
+      array_push(inside_vertices, intersection_point);
+    }
+    if (current_dot > 0.0f) {
+      array_push(inside_vertices, *current_vertex);
+    }
+    // move to next vertex
+    previous_dot = current_dot;
+    previous_vertex = current_vertex;
+    current_vertex++;
+  }
+  // free existing vertices and replace with new vertices
+  array_free(polygon->vertices);
+  polygon->vertices = inside_vertices;
 }
 
 void clip_polygon_against_frustum(
